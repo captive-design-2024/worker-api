bd6198671f6fb50a55a91688c8e1363d909a7fe5
jaeyong <ljy3573507@gmail.com>
2024-10-08 14:47:07 +0900

    llm controller

--- src/llm/llm.controller.ts
+++ src/llm/llm.controller.ts
@@ -17,7 +17,17 @@ export class LlmController {
 
   @Post('check')
   async check(@Body('content') content: string) {
-    console.log(content + '위는 오디오 추출로 만든 자막인데 문맥적으로 어색한 부분에 밑줄쳐줘')
+    // console.log(content + '위는 오디오 추출로 만든 자막인데 문맥적으로 어색한 부분에 밑줄쳐줘')
     return this.messageService.sendMessage(content + '위는 오디오 추출로 만든 SRT파일이야. 내용에서 문맥적으로 어색한 부분에 밑줄쳐주고 형식은 SRT로 해줘');
+  }
+
+  @Post('recommend')
+  async recommendTitle(@Body('content') content: string) {
+    return this.messageService.sendMessage(content + '위는 오디오 추출로 만든 SRT파일이야. 내용을 바탕으로 제목과 해시태그 추천해줘. 형식은 제목:답변, 해시태그1:답변, 해시태그2: 답변 이런식으로 해줘');
+  }
+
+  @Post('translate')
+  async translate(@Body('content') content: string, @Body('language') language: string) {
+    return this.messageService.sendMessage(content + '위는 오디오 추출로 만든 SRT파일이야. 내용을 ' + language + '로 번역해줘. 형식은 SRT파일을 유지해줘');
   }
 }
